42732bf012482d85ee7e05865733738b9f0dcde7
LiuZhaoHui <LiuZhaoHui@ithinker.com.cn>
2019-08-05 21:49:50 +0800

    产品分页

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,6 +10,8 @@ import { Product } from '../model/product.model';
 export class StoreComponent implements OnInit {
 
   public selectedCategory: string = null;
+  public productsPerPage: number = 4;
+  public selectedPage: number = 1;
 
   constructor(private productRepository: ProductRepository) {}
 
@@ -17,14 +19,34 @@ export class StoreComponent implements OnInit {
   }
 
   get products(): Product[] {
-    return this.productRepository.getProducts(this.selectedCategory);
+    var pageIndex = this.productsPerPage * (this.selectedPage - 1);
+    console.log(pageIndex);
+    return this.productRepository.getProducts(this.selectedCategory)
+      .slice(pageIndex, this.productsPerPage + pageIndex);
   }
 
   get categories(): string[] {
     return this.productRepository.getCategories();
   }
+  
+  get pageNumbers(): number[]  {
+    var total = this.productRepository.getProducts(this.selectedCategory).length;
+    return Array(Math.ceil(total / this.productsPerPage)).fill(0).map((x, i) => i + 1);
+  }
 
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
+    this.changePage(1);
   }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = newSize;
+    this.changePage(1);
+  }
+
+  changePage(newPage: number){
+    this.selectedPage = newPage;
+  }
+
+  
 }
